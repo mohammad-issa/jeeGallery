6ce6c97a8d19ce366546128f07f1071683b649e7
Mohammad issa <m.issa@mxd3d.com>
2017-12-21 18:11:36 +0200

    FlowRouter

---
+++ jeeGallery/imports/ui/components/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor'
+
+export default class Header extends Component {
+
+  render() {
+    return (
+      <h1>Header</h1>
+    );
+  }
+}
